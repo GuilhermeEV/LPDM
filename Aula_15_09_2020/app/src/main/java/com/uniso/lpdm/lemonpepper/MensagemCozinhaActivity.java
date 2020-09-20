1cd391a296b417959fc37817f5960bf367d59c9d
GuilhermeEV <guilhermeev3@gmail.com>
2020-09-20 18:21:15 -0300

    Exercicio da aula do dia 15/09

---
+++ Aula_15_09_2020/app/src/main/java/com/uniso/lpdm/lemonpepper/MensagemCozinhaActivity.java
@@ -0,0 +1,32 @@
+package com.uniso.lpdm.lemonpepper;
+
+/*Importações*/
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MensagemCozinhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensagem_cozinha);
+        }
+
+        /*Botão de Enviar a Mensagem para o Whatsapp (compartilhar), onde a mesma cria um intent que pega a mensagem e converte para string*/
+    public void onEnviarMensagemWhatsapp(View view){
+        EditText editText = (EditText) findViewById(R.id.editMensagem);
+        String mensagem = editText.getText().toString();
+
+        Intent intencao = new Intent(Intent.ACTION_SEND);
+        intencao.setType("text/plain");
+        intencao.putExtra(Intent.EXTRA_TEXT, mensagem);
+        intencao.putExtra(Intent.EXTRA_SUBJECT, "Assunto");
+
+        startActivity(intencao);
+    }
+}
